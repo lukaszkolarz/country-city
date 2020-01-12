687b0bf55167bf5e393487c8c0112951e5550c04
XEQer <59603258+XEQer@users.noreply.github.com>
2020-01-12 13:02:01 +0100

    Add files via upload

--- Server.py
+++ Server.py
@@ -2,49 +2,97 @@ import socket as sc
 import threading
 from _thread import *
 import random
+import logging as log
+from G_server import G_server
+import pickle
+
+def send_pickle(data):
+    clientsocket.send(pickle.dumps(data))
+
+def send(data, headerSize=10):  # sending to server
+    data = f'{len(data):<{headerSize}}' + data
+    clientsocket.send(data)
+
+def recv(headerSize=10):  # receiving from server
+    full_msg = ""
+    new_msg = True
+    while True:
+        msg = clientsocket.recv(16)
+        if new_msg:
+            msg_len = int(msg[:headerSize])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - headerSize == msg_len:
+            full_msg = full_msg[headerSize:]
+            break
+    return full_msg
 def generator():
     marks = "abcdefghijklmnouprstuvwyz"
     k = len(marks)
     generate = random.randint(0, k)
     letter = marks[generate]
     return letter
+#def send(msg):
+ #   msg = f"{len(msg):<{HEADSIZE}}" + msg
+  #  clientsocket.send(bytes(msg,"utf-8"))
+
+
+log.basicConfig(filename="server.log",level=log.DEBUG)
+k = G_server()
 server = "172.19.127.251"
-port = 8107
+port = 8108
 s = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
 s.bind((server,port))
 s.listen(5)
 print("Waiting for a connection")
 
+def threaded_client(clientsocket,player):
 
-def threaded_client(clientsocket, player):
-
-    clientsocket.send(str.encode("    Connected Jaki masz login"))
-    login = clientsocket.recv(1024) #tutaj powinien odebrac login
-    reply = login.decode("utf-8")
-    print(reply)
-    clientsocket.send(str.encode("Zacznijmy zabawe")) # wysłanie wiadomosci konczącej sesje powitalna
+    send(str(player))  #send(str(player))
+    login = recv() #tutaj powinien odebrac login
+    print(login)
+    send("Zacznijmy zabawę")     # wysłanie wiadomosci konczącej sesje powitalna
+    #reply = clientsocket.recv(1024).decode()
+    #print(reply)
     while True:
         try:
-            #print(clientsocket.recv(1024).decode("utf-8"))
-            clientsocket.send(str.encode( generator()))    #wysyłamy liczbę
-            print(clientsocket.recv(1024).decode("utf-8"))
+            v=send(generator())   #wysyłamy liczbę
+            print(v)
+            vector = pickle.loads((clientsocket.recv(1024)))
+            #for i in range(6):
+                #vector[i] =recv()
+            #k.append(vector)     #tutaj go łączy w wektor wektorów
 
-            vector = []
-            for i in range(5):
-                vector[i]=clientsocket.recv(1024).decode("utf-8")
-                print(clientsocket.send(str.encode(("wektor odebrany"))))
-            print(vector)
+            while True:
+                if k.size() == current_players - 1:     #spradza czy wszyscy wysłali
+                    break
 
-
-
-            if not login:
-                break
+            clientsocket.send(bytes(str(current_players),"utf-8"))
+            if player ==0:
+                for i in range(current_players):
+                    for j in range(6):
+                        clientsocket.send(bytes(k[i][j],"utf-8"))     #Nie wiem czy to działą , pcozekać na łukasza
             else:
-                print("Received:", reply)
-                print("Sending",reply)
+                clientsocket.send(bytes("Trwa sprawdzania wyników","utf-8"))
 
-            clientsocket.sendall(str.encode(reply))
+            if player == 0:
+                for i in range(current_players):
+                    for j in range(6):
+                        m =clientsocket.recv(1024).decode("utf-8")
+                        k[i][j] = m
+
+            k.check1()
+            k.check2()
+            k.create_score()
+            for i in range(current_players):
+                for j in range(2):
+                    clientsocket.send(bytes(k[i][j],"utf-8"))
+
+            k.clear()
         except:
+
             break
     print("Lost connection ")
     clientsocket.close()
@@ -53,7 +101,8 @@ current_players = 0
 while True:
     clientsocket , address = s.accept()
     print("Connection from :",address)
-    clientsocket.send(bytes("Connected with server","utf-8"))
+    log.info("Connected from" + str(address))
+    send("Connected with server")
     start_new_thread(threaded_client,(clientsocket,current_players))
     current_players +=1
 
