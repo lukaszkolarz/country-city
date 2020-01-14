ce1989b9fc3361c53cfc6daf2318d3ceedc11e98
lukaszkolarz <lukaszkolarz@gmail.com>
2020-01-14 11:57:38 +0100

    client log added

--- Client.py
+++ Client.py
@@ -35,17 +35,23 @@ while msg != "no":
 
     session.buildVector(country, city, animal, plant, item)
     session.sendVector(session.vector)
+    log.info("Answers sent")
 
     if int(session.ID) == 0:
         vector = session.recvCheck()
+        log.info("Received to check")
         #checking procedure
         session.sendCheckBack(vector)
+        log.info("Sent after checking")
     else:
         print(session.recvNotCheck())
+        log.info("Waiting for points")
 
     points = session.recvPoints()
+    log.info("Points received")
     #display points
     print("Your score" + session.findMyPoint() + "\n")
+    log.info("Player's points received")
     print("Would u like to play again? / \"no\" for not")
     msg = input()
 
