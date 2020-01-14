346ab3a5758d5cb896d230ca797a2733b276aacb
XEQer <59603258+XEQer@users.noreply.github.com>
2020-01-14 11:44:10 +0100

    Add files via upload

--- Server.py
+++ Server.py
@@ -3,49 +3,69 @@ import threading
 from _thread import *
 import random
 import logging as log
-from G_server import G_server
-
-# import pickle
-
-"""def send_pickle(data):
+#from G_server import G_server
+import pickle
+import numpy
+def send_pickle(data):
     clientsocket.send(pickle.dumps(data))
 def recv_pickle():
-    return pickle.loads(clientsocket.recv(2048)) """
-
-
+    vector = pickle.loads(clientsocket.recv(2048))
+    log.info(" answers sent")
+    return vector
 def send(data):  # sending to server
     clientsocket.send(bytes(data, "utf-8"))
-
-
 def recv():  # receiving from server
     msg = clientsocket.recv(2048).decode("utf-8")
     return msg
-
-
 def generator():
     marks = "abcdefghijklmnouprstuvwyz"
     k = len(marks)
     generate = random.randint(0, k)
     letter = marks[generate]
     return letter
+def check1(vector):
+    a = numpy.shape(vector)
+    for i in range(a[0]):
+        vector[i][6] = 0
+    for i in range(a[0]):
+        for j in range(1,6):
+            if vector[i][j] != 0:
+                vector[i][6] =+10
+    return vector
+def check2(vector):
+    a = numpy.shape(vector)
+    a = a[0]
+    for i in range(1, 6):
+        for j in range(a):
+            for n in range(a):  # sprawdzanie powtórzen
+                if j != n:
+                    if vector[j][i] == vector[j][n]:
+                        vector[j][6] -= 5
 
+    return vector
+def create_score(vector):
+    score = []
+    a = numpy.shape(vector)
+    a = a[0]
+    for i in range(a):
+        score[i][0] = vector[i][0]
+        score[i][1] = vector[i][6]
+    return score
 
-# def send(msg):
-#   msg = f"{len(msg):<{HEADSIZE}}" + msg
-#  clientsocket.send(bytes(msg,"utf-8"))
-
-
+global Vector
+global current_player
 log.basicConfig(filename="server.log", level=log.DEBUG)
-k = G_server()
-server = "192.168.1.45"
+#k = G_server()
+server = "172.19.127.251"
 port = 8000
-s = sc.socket(sc.AF_INET, sc.SOCK_SEQPACKET, sc.IPPROTO_SCTP)
+s = sc.socket(sc.AF_INET, sc.SOCK_STREAM, sc.IPPROTO_SCTP)
 s.bind((server, port))
 s.listen(5)
 print("Waiting for a connection")
 
 
 def threaded_client(clientsocket, player):
+    #global Vector
     send(str(player))  # send(str(player))
     login = recv()  # tutaj powinien odebrac login
     print(login)
@@ -56,41 +76,43 @@ def threaded_client(clientsocket, player):
             v = generator()
             send(v)  # wysyłamy liczbę
             print(v)
-            # vector = pickle.loads((clientsocket.recv(1024)))
-            for i in range(6):
-                vector = []
-                vector[i] = recv()
-                k.append(vector)  # tutaj go łączy w wektor wektorów
+            vector = []
+            vector = recv_pickle()
+            log.info(" answers received")
+            print(vector)
+            Vector = []
+            Vector.append(vector)
+            print(Vector)      # tutaj go łączy w wektor wektorów
+            a = numpy.shape(Vector)
 
             while True:
-                if k.size() == current_players - 1:  # spradza czy wszyscy wysłali
+                if a[0] == current_players  :  # spradza czy wszyscy wysłali
                     break
 
-            send(current_players)
             if player == 0:
-                # send_pickle(k)
-                for i in range(current_players):
-                    for j in range(6):
-                        clientsocket.send(k[i][j])  # Nie wiem czy to działą , pcozekać na łukasza"""
+                send_pickle(Vector)
+                log.info("Answers of all player was sent to client number 0 ")
             else:
-                clientsocket.send("Sprawdzanie wyników")
+                send("Sprawdzanie wyników")
+                log.info("Information sent")
 
             if player == 0:
-                # vector1 = recv_pickle()
-                # k.fill(vector1, current_players)
-                for i in range(current_players):
-                    for j in range(6):
-                        m = clientsocket.recv()
-                        k[i][j] = m
+                vector1 =[]
+                vector1 = recv_pickle()
+                log.info("Answers was chcecked and received from player number 0")
 
-            k.check1()
-            k.check2()
-            k.create_score()
-            for i in range(current_players):
+            Vector = check1(vector1)
+            Vector = check2(vector1)
+
+
+            Vector = create_score(Vector)
+            """for i in range(current_players):
                 for j in range(2):
-                    clientsocket.send(k[i][j])
+                    clientsocket.send(k[i][j])"""
+            send_pickle(Vector)
+            log.info("Score was sent to clients")
 
-            k.clear()
+            Vector = []
         except:
 
             break
